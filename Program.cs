5987b2806089191354f923537e48651629e167a6
alyilmaz99 <73197677+alyilmaz99@users.noreply.github.com>
2022-05-17 20:00:52 +0300

    test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Stripe;
+namespace stripeExample
+{
+    class Program
+    {
+        static void Main(string[] args){
+            
+
+            StripeConfiguration.ApiKey="sk_test_51Kyex0F4m0MKLar222Hi5t9qotB1pannBxt6FWPwzErbTHcC9TDRKRQxsZzvVm3BVHOgzdSiv6FC1zwRZxrozlIN00RWEHPvAr";
+            var service = new CustomerService();
+            var customers = service.List();
+            Console.WriteLine(customers);
+        }
+    }
+}
